bee24e7bac5a7beb50b977a881af95b25e89e07d
wk275 <55239601+wk275@users.noreply.github.com>
2022-12-31 13:20:33 +0100

    Delete docker_up_all.sh

--- docker_up_all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for item in `ls ~/qbTools/docker|grep qbus`
-do
-        echo "-------------------------------------"
-        cd ~/qbTools/docker/$item
-        docker compose up -d
-done
