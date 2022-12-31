e82293e7bcb83b0e4cf65e9a93e23089d572c785
wk275 <55239601+wk275@users.noreply.github.com>
2022-12-31 13:20:21 +0100

    Delete docker_rm_all.sh

--- docker_rm_all.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#set -x
-for item in `docker ps -a |awk 'NF>1{print $NF}'|grep qbus`
-do
-        echo "-------------------------------------"
-        cd ~/qbTools/docker/$item
-        docker compose rm --stop --force
-done
